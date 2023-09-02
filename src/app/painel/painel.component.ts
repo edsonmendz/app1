6fb2c707b522f12b02376d6eb6fc5d41e8bd99be
edsonmendz <japaact@gmail.com>
2023-09-02 01:17:12 -0300

    Event binding aula 105

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent {
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase'
 
+  constructor() {
+    console.log(this.frases)
+  }
+
+  public atualizaResposta(): void {
+    console.log('teste')
+  }
 }
